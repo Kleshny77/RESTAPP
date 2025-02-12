8ae88706f5e8d493b8754ae3b2dfb83fd5e9a546
Artem <samsonovartem00@gmail.com>
2025-02-12 19:27:13 +0300

    Initial Commit

---
+++ RESTAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RESTAPP
+//
+//  Created by Artem Samsonov on 12.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
